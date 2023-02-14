3dbdb2407bd4e50d8eb99f0ba11996ddd40a667f
a.ranjbar <amin7ranjbar@gmail.com>
2023-02-14 16:24:27 +0330

    add test data

--- src/mock-vote.js
+++ src/mock-vote.js
@@ -4,6 +4,15 @@ const PARTY_COUNT = 3;
 const SEATS_COUNT = 100;
 
 const parties = [];
+let overMajorityParty = [];
+const testData = [
+  { vote: [0, 1, 2], count: 425889 },
+  { vote: [0, 2, 1], count: 77047 },
+  { vote: [1, 0, 2], count: 11737 },
+  { vote: [1, 2, 0], count: 31 },
+  { vote: [2, 0, 1], count: 86142 },
+  { vote: [2, 1, 0], count: 69243 },
+];
 
 for (let index = 0; index < PARTY_COUNT; index++) {
   parties.push({
@@ -11,8 +20,46 @@ for (let index = 0; index < PARTY_COUNT; index++) {
     name: String.fromCharCode(index + 65),
     votes: 0,
     seats: 0,
+    percent: 0,
   });
 }
+
+const deletedParties = [];
+
+const rankedVotes = testData
+  ? testData
+  : generatePermutations(parties.map((item) => item.index)).map((item) => {
+      const random = Math.floor(Math.random() * 100000);
+      return { vote: item, count: random };
+    });
+
+const electorates = rankedVotes.reduce((a, v) => a + v.count, 0);
+
+resetVotes();
+
+const overMajorityVotes = MAJORITY_LIMIT * electorates;
+const underMinorityVotes = MINORITY_LIMIT * electorates;
+
+overMajorityParty = parties.filter((item) => item.votes > overMajorityVotes);
+
+console.log(overMajorityParty);
+console.log(overMajorityVotes, underMinorityVotes);
+console.log(electorates);
+
+console.log(parties);
+
+overMajorityParty.map((item) => deletedParties.push(item.index));
+resetVotes();
+console.log(parties);
+
+const underMinorityParty = parties.filter(
+  (item) => parseFloat(item.percent) < MINORITY_LIMIT,
+);
+
+underMinorityParty.map((item) => deletedParties.push(item.index));
+resetVotes();
+console.log(parties);
+
 function generatePermutations(list, size = list.length) {
   if (size > list.length) return [];
   else if (size == 1) return list.map((d) => [d]);
@@ -24,38 +71,25 @@ function generatePermutations(list, size = list.length) {
   );
 }
 
-const rankedVotes = generatePermutations(parties.map((item) => item.index)).map(
-  (item) => {
-    const random = Math.floor(Math.random() * 1000);
-    return { vote: item, count: random };
-  },
-);
-
-const electorates = rankedVotes.reduce((a, v) => a + v.count, 0);
-
-rankedVotes.map((item) => {
-  parties[item.vote[0]].votes += item.count;
-});
-
-const overMajorityVotes = MAJORITY_LIMIT * electorates;
-const underMinorityVotes = MINORITY_LIMIT * electorates;
-
-const overMajorityParty = parties.filter(
-  (item) => item.votes > overMajorityVotes,
-);
-
-const underMinorityParty = parties.filter(
-  (item) => item.votes < underMinorityVotes,
-);
-
-console.log(underMinorityParty);
-console.log(overMajorityParty);
-
-if (overMajorityParty.length > 0) {
-  console.log(overMajorityParty);
-} else {
-  console.log(overMajorityVotes, underMinorityVotes);
-  console.log(electorates);
-  console.log(rankedVotes);
-  console.log(parties);
+function resetVotes() {
+  for (const party of parties) {
+    party.votes = 0;
+  }
+  rankedVotes.map((item) => {
+    for (const vote of item.vote) {
+      if (deletedParties.includes(vote)) {
+        continue;
+      } else {
+        parties[vote].votes += item.count;
+        break;
+      }
+    }
+  });
+  for (const party of parties) {
+    if (overMajorityParty.length > 0) {
+      party.percent = (party.votes / electorates / 4).toFixed(2);
+    } else {
+      party.percent = (party.votes / electorates).toFixed(2);
+    }
+  }
 }
